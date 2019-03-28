8b8da23f4aabcde6d6a967ddfff925e43b49fff4
Willemecloutier26 <willemecloutier@hotmail.com>
2019-03-28 16:20:12 -0400

    Programmation Morse

---
+++ morse.py
@@ -0,0 +1,58 @@
+import os
+
+message = input("Please enter your secret message")
+
+message.lower()
+for char in message:
+	if char == 'a':
+		os.system('python3 A.py')
+	elif char == 'b':
+		os.system('python3 b.py')
+	elif char == 'c':
+		os.system('python3 c.py')
+	elif char == 'd':
+		os.system('python3 d.py')
+	elif char == 'e':
+		os.system('python3 E.py')
+	elif char == 'f':
+		os.system('python3 F.py')
+	elif char == 'g':
+		os.system('python3 g.py')
+	elif char == 'h':
+		os.system('python3 h.py')
+	elif char == 'i':
+		os.system('python3 i.py')
+	elif char == 'j':
+		os.system('python3 j.py')
+	elif char == 'k':
+		os.system('python3 k.py')
+	elif char == 'l':
+		os.system('python3 l.py')
+	elif char == 'm':
+		os.system('python3 M.py')
+	elif char == 'n':
+		os.system('python3 n.py')
+	elif char == 'o':
+		os.system('python3 o.py')
+	elif char == 'p':
+		os.system('python3 p.py')
+	elif char == 'q':
+		os.system('python3 q.py')
+	elif char == 'r':
+		os.system('python3 r.py')
+	elif char == 's':
+		os.system('python3 s.py')
+	elif char == 't':
+		os.system('python3 t.py')
+	elif char == 'u':
+		os.system('python3 u.py')
+	elif char == 'v':
+		os.system('python3 v.py')
+	elif char == 'w':
+		os.system('python3 w.py')
+	elif char == 'x':
+		os.system('python3 x.py')
+	elif char == 'y':
+		os.system('python3 y.py')
+	elif char == 'z':
+		os.system('python3 z.py')
